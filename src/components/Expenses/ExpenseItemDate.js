988574b44cc979eb0cce823a08566a94f3cf41ad
SagarGYadav <118979610+SagarGYadav@users.noreply.github.com>
2022-12-02 20:26:43 +0530

    const and arrow function . and clickHandler event

--- src/components/Expenses/ExpenseItemDate.js
+++ src/components/Expenses/ExpenseItemDate.js
@@ -1,6 +1,6 @@
 import "./ExpenseItemDate.css";
 
-function ExpenseItemDate(props) {
+const ExpenseItemDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
