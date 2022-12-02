1e2ff3e0db4d944bba8aabdbefdc31f90a4ab657
SagarGYadav <118979610+SagarGYadav@users.noreply.github.com>
2022-12-02 14:02:33 +0530

    Basics of React = props.children &  card

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import ExpenseItemDate from "./ExpenseItemDate";
 import "./ExpenseItem.css";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseItemDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
