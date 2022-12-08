c3d6e8ce893eeecdc78332c057f4cf4f81d77582
SagarGYadav <118979610+SagarGYadav@users.noreply.github.com>
2022-12-08 17:45:46 +0530

    filteredExpenses(Expenses.js)

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ const DUMMY_EXPENSES = [
     id: "e1",
     title: "Toilet paper",
     amount: 94.12,
-    date: new Date(2020, 7, 14),
+    date: new Date(2022, 1, 20),
   },
   {
     id: "e2",
     title: "Football",
     amount: 24.12,
-    date: new Date(2020, 8, 4),
+    date: new Date(2021, 10, 12),
   },
   {
     id: "e3",
     title: "New TV",
     amount: 794.92,
-    date: new Date(2020, 8, 12),
+    date: new Date(2021, 8, 4),
   },
   {
     id: "e4",
     title: "Car Insurance",
     amount: 348.12,
-    date: new Date(2020, 10, 20),
+    date: new Date(2021, 7, 14),
   },
 ];
 
