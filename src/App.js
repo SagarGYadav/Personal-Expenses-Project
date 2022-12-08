594cfc59a2dc9df501be0b22f31dd6826008e25e
SagarGYadav <118979610+SagarGYadav@users.noreply.github.com>
2022-12-08 17:14:59 +0530

    map method (in Expenses.js) and useState (in App.js)

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "Football",
+    amount: 24.12,
+    date: new Date(2020, 8, 4),
+  },
+  {
+    id: "e3",
+    title: "New TV",
+    amount: 794.92,
+    date: new Date(2020, 8, 12),
+  },
+  {
+    id: "e4",
+    title: "Car Insurance",
+    amount: 348.12,
+    date: new Date(2020, 10, 20),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "Football",
-      amount: 24.12,
-      date: new Date(2020, 8, 4),
-    },
-    {
-      id: "e3",
-      title: "New TV",
-      amount: 794.92,
-      date: new Date(2020, 8, 12),
-    },
-    {
-      id: "e4",
-      title: "Car Insurance",
-      amount: 348.12,
-      date: new Date(2020, 10, 20),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
